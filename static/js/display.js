822a4d48c18acd8dd85c886db2908a9f24efef7c
Jason D. Balkman <jshining@gmail.com>
2014-12-07 19:35:01 -0500

    breast cases

--- static/js/display.js
+++ static/js/display.js
@@ -1,11 +1,29 @@
 $(function(){
 
-    // Globals
+   $("#example-1").on("click",function(e) {
+       e.preventDefault();
+       jwplayer("myVideoPlayer").load([{file:"/content/BreastCase1.mp4", image:"/content/Slide1.jpg"}]);
+       //jwplayer("myVideoPlayer").setup([{file:"/content/Example.mp4"}]); // try setup?
+   }); 
 
-    var curr_file = "uploads/IM-0001-4035.tif";
+   $("#example-2").on("click",function(e) {
+       e.preventDefault();
+       jwplayer("myVideoPlayer").load([{file:"/content/BreastCase2.mp4", image:"/content/Slide2.jpg"}]);
+   }); 
 
-    function display_main(msg) {
-	alert("Called Display script:"+msg);
-    }
+   $("#example-3").on("click",function(e) {
+       e.preventDefault();
+       jwplayer("myVideoPlayer").load([{file:"/content/BreastCase3.mp4", image:"/content/Slide3.jpg"}]);
+   }); 
+
+   $("#example-4").on("click",function(e) {
+       e.preventDefault();
+       jwplayer("myVideoPlayer").load([{file:"/content/BreastCase4.mp4", image:"/content/Slide4.jpg"}]);
+   }); 
+
+   $("#example-5").on("click",function(e) {
+       e.preventDefault();
+       jwplayer("myVideoPlayer").load([{file:"/content/BreastCase5.mp4", image:"/content/Slide5.jpg"}]);
+   });
 
 });
